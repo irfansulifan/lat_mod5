b8c8b136d3c3668ca380a976cc72e8a48604b2b5
irfansulifan <127457127+irfansulifan@users.noreply.github.com>
2023-11-28 14:04:27 +0700

    Add files via upload

---
+++ weblp5-main/weblp5-main/app/Config/DatabaseConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig
+{
+
+    // SETTING KONFIGURASI DATABASE KALIAN
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "latihan_prak";
+    public $port = 3306;
+}
